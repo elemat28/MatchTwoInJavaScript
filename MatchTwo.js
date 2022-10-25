bb2b74140254ebe338d152d72779dd280e9a014e
Mateusz Markowski <elemat28@gmail.com>
2022-10-25 21:00:02 +0100

    Game logic now functional

--- MatchTwo.js
+++ MatchTwo.js
@@ -1,13 +1,51 @@
 console.log("Hello World!");
 let currentClick = null;
+let firstClicked = null;
+let secondClicked = null;
+let matchesRequired = 6;
+let matchesCurrently = 0;
+let timeout = null;
 //console.log(document.getElementById("C1").textContent);
 
 function clickCard(cardID){
+    //ignore the click if card already facing up
+    if(document.getElementById(cardID).firstChild.style.display == "inline"){
+        return;
+    }
+    //unflip cards if next click happens before previously clicked cards dissapear on their own
+    if(secondClicked != null){
+        clearTimeout(timeout);
+        unflipCards();
+    }
     currentClick = cardID;
     let element = document.getElementById(cardID);
     element.firstChild.style.display = "inline";
     console.log("Currently clicked ID: " + document.getElementById(cardID).id);
-
+    if(firstClicked == null){
+        firstClicked = currentClick;
+        console.log("First Clicked updated");
+        currentClick = null;
+    }
+    else {
+        if(secondClicked == null){
+            secondClicked = currentClick;
+            currentClick = null;
+            if(document.getElementById(firstClicked).textContent == document.getElementById(secondClicked).textContent){
+                //if first and second clicked tiles contain the same emoji
+                //add a match and clear history of clicks
+                matchesCurrently +=1;
+                console.log("Current Number Of matches: "+matchesCurrently);
+                firstClicked = secondClicked = null;
+                
+            } else {
+                console.log("Create Timeout")
+                timeout = setTimeout(unflipCards, 1500);
+            }
+            
+        }
+        
+        
+    }
 }
 
 function printContentToConsole(text){
@@ -22,7 +60,6 @@ function getAllCardIDs() {
         const element = children[index];
         IDsArr.push(element.id);
     };
-    console.log("IDsArr: "+ IDsArr);
     return IDsArr;
 }
 
@@ -37,37 +74,15 @@ function assignOnClickToAllCards(cardIDs){
     cardIDs.forEach(createOnClick);
 }
 
-let CardIDs = getAllCardIDs();
-
-
-class GameConductor {
-    constructor(){
-        this.firstClicked = null;
-        this.secondClicked = null;
-        this.matchesRequired = null;
-        this.matchesCurrently = null;
-    }
-    startGame(){
-        while(this.matchesCurrently < this.matchesRequired){
-            if(currentClick == null){
-                
-            }
-            else {
-                if(this.firstClicked == null){
-                    this.firstClicked = currentClick;
-                }
-                else {
-                    if(this.secondClicked == null){
-
-                    }
-                    
-                }
-            }
-        }
-    }
+function unflipCards(){
+    document.getElementById(firstClicked).firstChild.style.display = "none";
+    document.getElementById(secondClicked).firstChild.style.display = "none";
+    firstClicked = secondClicked = null;
+    
 }
 
-let GameEngine = new GameConductor();
+
+
 let cardIDs = getAllCardIDs();
 assignOnClickToAllCards(CardIDs);
 
@@ -78,3 +93,6 @@ document.getElementById(CID).onclick = function() {
     clickCard(CID);
 };
 */
+
+
+//setTimeout(this.updateLogic(), 100);

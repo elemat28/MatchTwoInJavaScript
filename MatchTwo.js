680cb069181102c006d343df1caf71f2c870bcd3
Mateusz Markowski <elemat28@gmail.com>
2022-10-25 18:18:33 +0100

    Cards log their value to console when clicked

--- MatchTwo.js
+++ MatchTwo.js
@@ -1,1 +1,48 @@
 console.log("Hello World!");
+//console.log(document.getElementById("C1").textContent);
+
+function clickCard(cardID){
+    console.log(document.getElementById(cardID).textContent);
+}
+
+function printContentToConsole(text){
+    console.log(text);
+}
+
+function getAllCardIDs() {
+    let IDsArr = [];
+    let GameGrid = document.getElementById("CardGrid");
+    let children = GameGrid.children;
+    for (let index = 0; index < children.length; index++) {
+        const element = children[index];
+        IDsArr.push(element.id);
+    };
+    console.log("IDsArr: "+ IDsArr);
+    return IDsArr;
+}
+
+function createOnClick(ID) {
+    document.getElementById(ID).onclick = function() {
+        clickCard(ID);
+    };
+
+}
+
+function assignOnClickToAllCards(cardIDs){
+    cardIDs.forEach(createOnClick);
+}
+
+let CardIDs = getAllCardIDs();
+assignOnClickToAllCards(CardIDs);
+
+
+
+
+
+/*
+let CID = "C1";
+let C1 = document.getElementById(CID);
+document.getElementById(CID).onclick = function() {
+    clickCard(CID);
+};
+*/

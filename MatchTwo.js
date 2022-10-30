36697c7d9cfd15032927b0c3ca1cf2946beac635
Mateusz Markowski <elemat28@gmail.com>
2022-10-30 15:05:11 +0000

    Added randomized emojis and a 'You won!' pop-up

--- MatchTwo.js
+++ MatchTwo.js
@@ -2,9 +2,14 @@ console.log("Hello World!");
 let currentClick = null;
 let firstClicked = null;
 let secondClicked = null;
+let numOfCards = 0;
 let matchesRequired = 6;
 let matchesCurrently = 0;
 let timeout = null;
+let timeoutMs = 1500;
+let GameGrid = document.getElementById("CardGrid");
+let defaultTheme = {start: 128512,
+                    end: 128580};
 //console.log(document.getElementById("C1").textContent);
 
 function clickCard(cardID){
@@ -34,18 +39,22 @@ function clickCard(cardID){
                 //if first and second clicked tiles contain the same emoji
                 //add a match and clear history of clicks
                 matchesCurrently +=1;
-                console.log("Current Number Of matches: "+matchesCurrently);
                 firstClicked = secondClicked = null;
                 
             } else {
                 console.log("Create Timeout")
-                timeout = setTimeout(unflipCards, 1500);
+                timeout = setTimeout(unflipCards, timeoutMs);
             }
             
         }
         
-        
+
     }
+    
+}
+
+function checkIfWon(){
+    window.alert("You won!");
 }
 
 function printContentToConsole(text){
@@ -66,6 +75,10 @@ function getAllCardIDs() {
 function createOnClick(ID) {
     document.getElementById(ID).onclick = function() {
         clickCard(ID);
+        console.log("matches currently vs required: "+ matchesCurrently + " : " + matchesRequired);
+        if(matchesCurrently == matchesRequired) {
+            timeout = setTimeout(checkIfWon, 75);
+        }
     };
 
 }
@@ -82,9 +95,62 @@ function unflipCards(){
 }
 
 
+function createGameBoard(size = 'm', difficulty = 'm'){
+    let IDsArr = [];
+    
+    let children = GameGrid.children;
+    if(size == 'm'){
+        numOfCards = 12;
+    }
+    for (let index = 0; index < numOfCards; index++) {
+        //const element = array[index];
+        let card = document.createElement("card");
+        card.id = 'C' + index;
+        let cardContent = document.createElement("cardImage");
+        card.appendChild(cardContent);
+        GameGrid.appendChild(card);
+    }
 
+    let cardIDs = getAllCardIDs();
+    
+    if(difficulty == 'm'){
+        matchesRequired = cardIDs.length/2;
+    }
+    let numOfImgs = cardIDs.length/2;
+    console.log("Number of emojis to use: "+ numOfImgs);
+    //Assign emoji images to each of the cards
+    let emojisToUse = [];
+    let range = (defaultTheme.end+1) - defaultTheme.start;
+    while(emojisToUse.length < numOfImgs){
+        let candidateEmoji = Math.floor(Math.random()*range) + defaultTheme.start;
+        /* If emoji isn't alerady in the list(is't a duplicate), add it to the list of emojis to use */
+        console.log(emojisToUse);
+        if(emojisToUse.indexOf(candidateEmoji) == -1){
+            emojisToUse.push(candidateEmoji);
+        }
+        console.log("Emojis chosen(len["+emojisToUse.length+"]): "+emojisToUse);
+    }
+    for (let index = 0; index < emojisToUse.length; index++) {
+        console.log("Assigning emoji: '"+ String.fromCodePoint(emojisToUse[index]) +"' to: ");
+        var cardIndex = Math.floor(Math.random()*cardIDs.length);
+        var tempID = cardIDs[cardIndex];
+        console.log(tempID);
+        document.getElementById(tempID).firstElementChild.textContent = String.fromCodePoint(emojisToUse[index]);
+        cardIDs.splice(cardIndex, 1);
+        cardIndex = Math.floor(Math.random()*cardIDs.length);
+        tempID = cardIDs[cardIndex];
+        console.log("and: ");
+        console.log(tempID);
+        document.getElementById(tempID).firstElementChild.textContent = String.fromCodePoint(emojisToUse[index]);
+        cardIDs.splice(cardIndex, 1);
+        //https://www.delftstack.com/howto/javascript/javascript-pick-random-from-array/#:~:text=Pick%20a%20Random%20Value%20From%20an%20Array%20Using,of%200%20to%20the%20length%20of%20the%20array.
+        
+        
+    }
+}
+createGameBoard();
 let cardIDs = getAllCardIDs();
-assignOnClickToAllCards(CardIDs);
+assignOnClickToAllCards(cardIDs);
 
 /*
 let CID = "C1";
